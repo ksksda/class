d859637a7d4750ed330d33d12dc6093d5a3fa50f
ksksda <33217713+ksksda@users.noreply.github.com>
2018-11-28 20:51:01 +0900

    Update easyC.c

--- easyC.c
+++ easyC.c
@@ -3,40 +3,35 @@
 #include<math.h>
 #include<ctype.h>
 int main(){
- char formula[256]={0};
- double a=0,b=1,c=1;
- int d=0,f=0;
- while(1){
-  scanf("%s",formula);
-  if(formula[0]=='+'||formula[0]=='-'||formula[0]=='*'||formula[0]=='/')b=a;
-  else b=1;
-  c=1;
-  a=d=f=0;
-  for(int i=0;formula[i]!='\0';i++){
-   switch(formula[i]){
-    case '-':c*=-1;
-    case '+':a+=b;b=1;break;
-    case '/':d=1;break;
-    case 's':f=1;break;
-    case 'c':f=2;break;
-    case 't':f=3;break;
+ char X[255],d,f,i;
+ double a=0,b,c,A=M_PI/180;
+ while(c=1){
+  scanf("%s",X);
+  b=(X[0]=='+'|X[0]=='-'|X[0]=='*'|X[0]=='/')?a:1;
+  for(a=d=f=i=0;X[i];i++){
+   switch(X[i]){
+    case'-':c*=-1;
+    case'+':a+=b;b=1;break;
+    case'/':d=1;break;
+    case't':f++;
+    case'c':f++;
+    case's':f++;
    }
-   if(isdigit(formula[i])){
-    c*=atof(&formula[i]);
+   if(isdigit(X[i])){
+    c*=atof(&X[i]);
     switch(f){
-    case 1:c=sin(c*M_PI/180);break;
-    case 2:c=cos(c*M_PI/180);break;
-    case 3:c=tan(c*M_PI/180);break;
+     case 1:c=sin(c*A);break;
+     case 2:c=cos(c*A);break;
+     case 3:c=tan(c*A);break;
     }
-    b*=(d? (1/c):c);
+    b*=(d?(1/c):c);
     c=1;
     d=f=0;
-    for(;isdigit(formula[i])||formula[i]=='.';i++);
+    while(isdigit(X[++i]));
     i--;
    }
   }
-  a+=b;
-  printf("=%lf\n",a);
+  printf("=%lf\n",a+=b);
  }
  return 0;
 }
